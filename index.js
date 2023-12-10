be666bcbb0eba2283631f61d21ffe43ef2e43f92
JasonLei <jlei@activision.com>
2023-12-10 13:13:02 -0800

    add index.js

---
+++ index.js
@@ -0,0 +1,19 @@
+/* Express Setup */
+const express = require('express');
+const app = express();
+
+app.use(express.static(__dirname));
+
+const bodyParser = require('body-parser');
+const expressSession = require('express-session')({
+  secret: 'secret',
+  resave: false,
+  saveUninitialized: false
+});
+
+app.use(bodyParser.json());
+app.user(bodyParser.urlencoded({ extended: true }));
+app.use(expressSession);
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => console.log(`App listenin on port ${port} `));
